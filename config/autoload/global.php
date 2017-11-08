702c8fad85163b3e52335914cf1060ca8e588946
Tashi Wangchuk <tttwangchuk@gmail.com>
2017-11-08 12:28:51 +1100

    Update global.php

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 return [
     'db' => [
         'driver' => 'Pdo',
-        //'dsn'    => sprintf('sqlite:%s/data/zftutorial.db', realpath(getcwd())),
+        //'dsn'    => sprintf('sqlite:%s/data/apir.db', realpath(getcwd())),
         'dsn'            => 'mysql:dbname=apir;host=localhost',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
